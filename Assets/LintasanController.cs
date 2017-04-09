d15d1d1c71f3a28bc07fafc0665b3d0b1b289e9e
13514103 muhammadreifiza <13514103@std.stei.itb.ac.id>
2017-04-09 13:56:15 +0700

    init commit

---
+++ Assets/LintasanController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LintasanController : MonoBehaviour {
+    private bool isValid = false;
+    private bool isInputDown = false;
+    public PlayerController player;
+    private Collider2D col;
+
+    void Start()
+    {
+        Collider2D col = GetComponent<Collider2D>();
+    }
+
+	// Use this for initialization
+	void Update () {
+        isInputDown = player.valid;
+        Debug.Log(string.Format("valid = {0}", isValid && isInputDown));
+	}
+	
+	void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log(col.gameObject.tag);
+        if (col.gameObject.CompareTag("Player"))
+        {
+            isValid = true;
+        }
+    }
+    
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            isValid = false;
+        }
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private bool isValid = false;
+    public bool valid
+    {
+        get
+        {
+            return isValid;
+        }
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Pindahin posisi sprite player tersembunyi ke kordinat posisi input
+        if (Input.GetMouseButton(0))
+        {
+            isValid = true;
+            Vector3 _temp = transform.position;
+            Vector3 _pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 _moveTo = new Vector3(_pos.x, _pos.y);
+            transform.position = Vector3.Lerp(_temp, _moveTo, Time.deltaTime*10);
+        }
+        else
+            isValid = false;
+	}
+
+}
